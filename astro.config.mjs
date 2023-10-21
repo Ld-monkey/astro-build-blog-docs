ab805f203ec1d8dd58a5747a4640b1ef43be1358
Ld-monkey <lfourteau91@gmail.com>
2023-10-21 15:30:25 +0200

    static github website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Ld-monkey.github.io',
+  base: '/astro-build-blog-docs',
+});
